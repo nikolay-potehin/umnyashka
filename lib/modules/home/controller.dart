c9464ac3474d12222d6c9da442c5fafda48610bb
nikolay-potehin <potehinnikolay@yandex.ru>
2024-02-09 08:37:12 +0500

    lessons renamed

--- lib/modules/home/controller.dart
+++ lib/modules/home/controller.dart
@@ -5,18 +5,18 @@ import 'package:umnyashka/data/models/ucard.dart';
 class HomeController extends GetxController {
   final lessons = [
     UCardData(
-      title: 'Самый тяжелый',
-      description: 'Выбери самые тяжелые предметы',
+      title: 'Легкий - Тяжелый',
+      description: 'Выбери правильные предметы',
       color: const Color.fromRGBO(80, 200, 120, 1),
     ),
     UCardData(
-      title: 'Самый маленький',
-      description: 'Выбери самый маленький предмет',
+      title: 'Маленький - Большой',
+      description: 'Выбери правильные предметы',
       color: const Color.fromRGBO(235, 76, 66, 1),
     ),
     UCardData(
-      title: 'Самый широкий',
-      description: 'Выбери самые широкие предметы',
+      title: 'Узкий - Широкий',
+      description: 'Выбери правильные предметы',
       color: const Color.fromRGBO(148, 87, 235, 1),
     ),
   ];
